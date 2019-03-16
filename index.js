3f9c1e8b7b975586f1f0c39e95395469b67432b2
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-03-16 19:41:56 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update multistream from v1.x to v2.x
    * remove JSCS as it's no longer maintained
    * update ESLint from v0.x to v5.x
    * update @shinnn/eslint-config and follow its rules, for example tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -3,93 +3,89 @@
 const multistream = require('multistream');
 
 function alwaysTrue() {
-  return true;
+	return true;
 }
 
 module.exports = function fallbackStream(sourceStreams, options) {
-  if (!Array.isArray(sourceStreams)) {
-    throw new TypeError(`${sourceStreams} is not an array. The first argument to fallback-stream must be an array.`);
-  }
+	if (!Array.isArray(sourceStreams)) {
+		throw new TypeError(`${sourceStreams} is not an array. The first argument to fallback-stream must be an array.`);
+	}
 
-  let filter;
-  options = options || {};
+	let filter;
+	options = options || {};
 
-  if (typeof options !== 'object' || options instanceof RegExp) {
-    options = {errorFilter: options};
-  }
+	if (typeof options !== 'object' || options instanceof RegExp) {
+		options = {errorFilter: options};
+	}
 
-  if (options.errorFilter) {
-    if (typeof options.errorFilter === 'function') {
-      filter = options.errorFilter;
-    } else {
-      if (options.errorFilter instanceof RegExp) {
-        filter = function(err) {
-          return options.errorFilter.test(err);
-        };
-      } else {
-        throw new TypeError(`Error filter must be a function or a regular expression, but it was ${typeof options.errorFilter}.`);
-      }
-    }
-  }
+	if (options.errorFilter) {
+		if (typeof options.errorFilter === 'function') {
+			filter = options.errorFilter;
+		} else if (options.errorFilter instanceof RegExp) {
+			filter = function(err) {
+				return options.errorFilter.test(err);
+			};
+		} else {
+			throw new TypeError(`Error filter must be a function or a regular expression, but it was ${typeof options.errorFilter}.`);
+		}
+	}
 
-  filter = filter || alwaysTrue;
+	filter = filter || alwaysTrue;
 
-  const result = multistream(sourceStreams.map((stream, index) => {
-    return () => {
-      if (typeof stream === 'function') {
-        stream = stream();
-        if (!stream || typeof stream.on !== 'function') {
-          throw new TypeError('All functions in the array must return a readable stream.');
-        }
-      } else if (!stream || typeof stream.on !== 'function') {
-        throw new TypeError('All items in the array must be a readable stream or a function.');
-      }
+	const result = multistream(sourceStreams.map((stream, index) => () => {
+		if (typeof stream === 'function') {
+			stream = stream();
+			if (!stream || typeof stream.on !== 'function') {
+				throw new TypeError('All functions in the array must return a readable stream.');
+			}
+		} else if (!stream || typeof stream.on !== 'function') {
+			throw new TypeError('All items in the array must be a readable stream or a function.');
+		}
 
-      if (sourceStreams.length === 1 || index === sourceStreams.length - 1) {
-        return stream;
-      }
+		if (sourceStreams.length === 1 || index === sourceStreams.length - 1) {
+			return stream;
+		}
 
-      let needsFallback = false;
-      const errorListeners = [];
+		let needsFallback = false;
+		const errorListeners = [];
 
-      stream.once('error', function cancelError(err) {
-        if (filter(err)) {
-          needsFallback = true;
-          result._errors.push(err);
-          stream.emit('end');
-          return;
-        }
+		stream.once('error', function cancelError(err) {
+			if (filter(err)) {
+				needsFallback = true;
+				result._errors.push(err); // eslint-disable-line no-underscore-dangle
+				stream.emit('end');
+				return;
+			}
 
-        for (const listener of [...errorListeners]) {
-          stream.on('error', listener);
-        }
+			for (const listener of [...errorListeners]) {
+				stream.on('error', listener);
+			}
 
-        stream.emit('error', err);
-      });
+			stream.emit('error', err);
+		});
 
-      stream.on('newListener', function removeDefaultErrorListeners(eventName, listener) {
-        if (eventName === 'error') {
-          errorListeners.push(listener);
-          return;
-        }
+		stream.on('newListener', function removeDefaultErrorListeners(eventName, newListener) {
+			if (eventName === 'error') {
+				errorListeners.push(newListener);
+				return;
+			}
 
-        if (eventName === 'close') {
-          for (const listener of errorListeners) {
-            stream.removeListener('error', listener);
-          }
-        }
-      });
+			if (eventName === 'close') {
+				for (const listener of [...errorListeners]) {
+					stream.removeListener('error', listener);
+				}
+			}
+		});
 
-      stream.once('end', () => {
-        if (!needsFallback) {
-          result._queue = [];
-        }
-      });
+		stream.once('end', () => {
+			if (!needsFallback) {
+				result._queue = []; // eslint-disable-line no-underscore-dangle
+			}
+		});
 
-      return stream;
-    };
-  }), options);
+		return stream;
+	}), options);
 
-  result._errors = [];
-  return result;
+	result._errors = []; // eslint-disable-line no-underscore-dangle
+	return result;
 };
--- test.js
+++ test.js
@@ -1,169 +1,196 @@
 'use strict';
 
-const fallbackStream = require('./');
-const from = require('from2-array');
+const fallbackStream = require('.');
+const {PassThrough, Readable} = require('stream');
 const test = require('tape');
-const through = require('through2');
+const toReadableStream = require('to-readable-stream');
 
-const tmpError = new Error();
+const tmpError = new Error('tmp error');
 
 function emitTmpErrorImmediately(stream) {
-  process.nextTick(() => stream.emit('error', tmpError));
-  return stream;
+	process.nextTick(() => stream.emit('error', tmpError));
+	return stream;
 }
 
 test('fallbackStream()', t => {
-  t.plan(22);
+	t.plan(15);
 
-  t.equal(fallbackStream.name, 'fallbackStream', 'must have a function name.');
+	const option = {};
 
-  const option = {};
+	fallbackStream([toReadableStream('a')], option)
+	.on('error', t.fail)
+	.on('data', data => {
+		t.equal(data.toString(), 'a', 'should create a readable stream.');
+		t.deepEqual(option, {}, 'should not modify the original option object.');
+	})
+	.on('end', function() {
+		t.deepEqual(
+			this._errors, // eslint-disable-line no-underscore-dangle
+			[],
+			'should set `_errors` property.'
+		);
+	});
 
-  fallbackStream([from('a')], option)
-    .on('error', t.fail)
-    .on('data', data => {
-      t.equal(data.toString(), 'a', 'should create a readable stream.');
-      t.deepEqual(option, {}, 'should not modify the original option object.');
-    })
-    .on('end', function() {
-      t.deepEqual(this._errors, [], 'should set `_errors` property.');
-    });
+	fallbackStream([toReadableStream('a'), toReadableStream('b')])
+	.on('error', t.fail)
+	.on('data', data => t.equal(
+		data.toString(),
+		'a',
+		'should use only the first stream when it doesn\'t emit any errors.'
+	));
 
-  fallbackStream([from('a'), from('b')])
-    .on('error', t.fail)
-    .on('data', data => t.equal(
-      data.toString(),
-      'a',
-      'should use only the first stream when it doesn\'t emit any errors.'
-    ));
+	fallbackStream([() => toReadableStream('a')])
+	.on('error', t.fail)
+	.on('data', data => t.equal(
+		data.toString(),
+		'a',
+		'should accept a function that returns a stream as a stream source.'
+	));
 
-  fallbackStream([from.bind(null, 'a')])
-    .on('error', t.fail)
-    .on('data', data => t.equal(
-      data.toString(),
-      'a',
-      'should accept a function that returns a stream as a stream source.'
-    ));
+	fallbackStream([() => emitTmpErrorImmediately(new PassThrough())])
+	.on('error', function(err) {
+		t.equal(err, tmpError, 'should emit an error when the last stream emits an error.');
+		t.deepEqual(
+			this._errors, // eslint-disable-line no-underscore-dangle
+			[],
+			'should not push the error to the `_errors` property when it\'s actually emitted.'
+		);
+	});
 
-  fallbackStream([emitTmpErrorImmediately.bind(null, through())])
-    .on('error', function(err) {
-      t.equal(err, tmpError, 'should emit an error when the last stream emits an error.');
-      t.deepEqual(
-        this._errors,
-        [],
-        'should not push the error to the `_errors` property when it\'s actually emitted.'
-      );
-    });
+	const willEmitError = toReadableStream('');
 
-  const willEmitError = from('');
+	fallbackStream([
+		willEmitError,
+		toReadableStream('a')
+	])
+	.on('error', t.fail)
+	.on('data', data => t.equal(
+		data.toString(),
+		'a',
+		'should use the next stream as a fallback when the current stream emits an error.'
+	))
+	.on('end', function() {
+		t.deepEqual(
+			this._errors, // eslint-disable-line no-underscore-dangle
+			[tmpError],
+			'should push the ignored errors to the `_errors` property.'
+		);
+	});
 
-  fallbackStream([
-    willEmitError,
-    from('a')
-  ])
-    .on('error', t.fail)
-    .on('data', data => t.equal(
-      data.toString(),
-      'a',
-      'should use the next stream as a fallback when the current stream emits an error.'
-    ))
-    .on('end', function() {
-      t.deepEqual(
-        this._errors,
-        [tmpError],
-        'should push the ignored errors to the `_errors` property.'
-      );
-    });
+	willEmitError.on('error', err => t.equal(
+		err,
+		tmpError,
+		'should not remove error event listeners explicitly added.'
+	));
+	willEmitError.emit('error', tmpError);
 
-  willEmitError.on('error', err => {
-    t.equal(err, tmpError, 'should not remove error event listeners explicitly added.');
-  });
-  willEmitError.emit('error', tmpError);
+	toReadableStream('a').on('end', function() {
+		fallbackStream([this, toReadableStream('b')], null)
+		.on('data', t.notOk)
+		.on('error', t.fail);
 
-  const alreadyEnded = from('a');
-  alreadyEnded.pipe(through());
-  alreadyEnded.on('end', () => {
-    fallbackStream([alreadyEnded, from('b')], null)
-      .on('data', t.notOk)
-      .on('error', t.fail);
+		t.ok(
+			this._readableState.ended, // eslint-disable-line no-underscore-dangle
+			'should stop fallback when the stream have already ended.'
+		);
+	}).pipe(new PassThrough());
 
-    t.ok(
-      alreadyEnded._readableState.ended,
-      'should stop fallback when the stream have already ended.'
-    );
-  });
+	fallbackStream([
+		() => emitTmpErrorImmediately(new PassThrough()),
+		new PassThrough()
+	], err => err.message === '__does_not_match__')
+	.on('error', err => {
+		t.deepEqual(
+			err,
+			tmpError,
+			'should use a function as an error filter.'
+		);
+	});
 
-  fallbackStream([
-    emitTmpErrorImmediately.bind(null, through()),
-    through()
-  ], err => err.message === '__does_not_match__')
-    .on('error', function(err) {
-      t.deepEqual(err, tmpError, 'should use a function as an error filter.');
-    });
+	fallbackStream([
+		() => new Readable({
+			read() {
+				process.nextTick(() => this.emit('error', new TypeError('error')));
+			}
+		}),
+		() => emitTmpErrorImmediately(new PassThrough()),
+		new PassThrough()
+	], /TypeError/u)
+	.on('error', err => {
+		t.equal(
+			err,
+			tmpError,
+			'should use a regular expression as an error filter.'
+		);
+	});
 
-  fallbackStream([
-    () => {
-      const stream = through();
-      process.nextTick(function() {
-        stream.emit('error', new TypeError());
-      });
-      return stream;
-    },
-    () => emitTmpErrorImmediately(through()),
-    through()
-  ], /TypeError/)
-    .on('error', err => {
-      t.equal(err, tmpError, 'should use a regular expression as an error filter.');
-    });
+	fallbackStream([
+		() => emitTmpErrorImmediately(new PassThrough({objectMode: true})),
+		new Readable({
+			objectMode: true,
+			read() {
+				this.push({a: 1});
+				this.push(null);
+			}
+		})
+	], {
+		objectMode: true,
+		errorFilter(err) {
+			t.equal(
+				err,
+				tmpError,
+				'should use `errorFilter` option as an error filter.'
+			);
 
-  fallbackStream([
-    () => emitTmpErrorImmediately(through.obj()),
-    from.obj({a: 1})
-  ], {
-    objectMode: true,
-    errorFilter(err) {
-      t.equal(err, tmpError, 'should use `errorFilter` option as an error filter.');
-      return true;
-    }
-  })
-    .on('error', t.fail)
-    .on('data', data => {
-      t.deepEqual(data, {a: 1}, 'should reflect readable stream options to the result.');
-    });
+			return true;
+		}
+	})
+	.on('error', t.fail)
+	.on('data', data => {
+		t.deepEqual(
+			data,
+			{a: 1},
+			'should reflect readable stream options to the result.'
+		);
+	});
+});
 
-  t.throws(
-    () => fallbackStream(from('a')),
-    /^TypeError.* is not an array.*must be an array/u,
-    'should throw a type error when the first argument is not an array.'
-  );
+test('Argument validation', t => {
+	t.throws(
+		() => fallbackStream(toReadableStream('a')),
+		/^TypeError.* is not an array.*must be an array/u,
+		'should throw a type error when the first argument is not an array.'
+	);
 
-  t.throws(
-    () => fallbackStream([() => {}]),
-    /^TypeError.*must return a readable stream/u,
-    'should throw a type error when the function returns a non-stream value.'
-  );
+	t.throws(
+		() => fallbackStream([() => {}]),
+		/^TypeError.*must return a readable stream/u,
+		'should throw a type error when the function returns a non-stream value.'
+	);
 
-  t.throws(
-    () => fallbackStream([{a: 1}, null]),
-    /^TypeError.*must be a readable stream or a function/u,
-    'should throw a type error when the array itemis neither a stream nor a function.'
-  );
+	t.throws(
+		() => fallbackStream([{a: 1}, null]),
+		/^TypeError.*must be a readable stream or a function/u,
+		'should throw a type error when the array itemis neither a stream nor a function.'
+	);
 
-  t.throws(
-    () => fallbackStream([from('a')], 'foo'),
-    /^TypeError.*it was string/u,
-    'should throw a type error when the second argument isn\'t a function/regexp/object.'
-  );
+	t.throws(
+		() => fallbackStream([toReadableStream('a')], 'foo'),
+		/^TypeError.*it was string/u,
+		'should throw a type error when the second argument isn\'t a function/regexp/object.'
+	);
 
-  t.throws(
-    () => fallbackStream([from('a')], {errorFilter: 1}),
-    /^TypeError.*it was number/u,
-    'should throw a type error when the `errorFilter` option isn\'t a regexp/function.'
-  );
+	t.throws(
+		() => fallbackStream([toReadableStream('a')], {errorFilter: 1}),
+		/^TypeError.*it was number/u,
+		'should throw a type error when the `errorFilter` option isn\'t a regexp/function.'
+	);
 
-  t.throws(
-    () => fallbackStream(),
-    /^TypeError.* is not an array.*must be an array/u,
-    'should throw a type error when it takes no arguments.'
-  );
+	t.throws(
+		() => fallbackStream(),
+		/^TypeError.* is not an array.*must be an array/u,
+		'should throw a type error when it takes no arguments.'
+	);
+
+	t.end();
 });
